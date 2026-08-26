ebee57cfb8a3f7a00f4809586b2937550a11b6f1
akelity <mich.murabito@gmail.com>
2026-08-26 07:08:13 +0000

    Deploying to gh-pages from @ CloudNativeDaysItaly/CloudNativeDaysItaly.github.io@6645c9b96f481d0f5572738cd82e474bad547871 🚀

---
+++ _next/static/chunks/app/brand-kit/page-f63155a50d06e240.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[89],{277:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23)),Promise.resolve().then(n.bind(n,1919)),Promise.resolve().then(n.bind(n,6622))}},e=>{e.O(0,[619,356,478,622,441,255,358],()=>e(e.s=277)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/content-hub/page-9d6b23a07a890d2c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[338],{6648:(e,s,_)=>{Promise.resolve().then(_.bind(_,2804))}},e=>{e.O(0,[619,356,804,441,255,358],()=>e(e.s=6648)),_N_E=e.O()}]);
